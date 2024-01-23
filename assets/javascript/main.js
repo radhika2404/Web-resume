ea1643c58e86f34782181c698dcea3fb9c701c0e
Radhika Mittal <113189137+radhika2404@users.noreply.github.com>
2024-01-23 20:18:10 +0530

    Add files via upload

---
+++ assets/javascript/main.js
@@ -0,0 +1,8 @@
+//adding jquery
+$(document).ready(function(){
+    $("#main-nav a").click(function(){
+        $("section").removeClass("show");
+        var target=$(this).attr("href");
+        $(target).addClass("show");
+    })
+});
